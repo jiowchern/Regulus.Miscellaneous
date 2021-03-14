cdd3a3b5b784c026daa51ba296ad3f7433338cf6
jc <jiowchern@gmail.com>
2021-03-14 15:50:35 +0800

    join behaviour quadtree.

---
+++ Regulus.BehaviourTree/ITicker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Regulus.BehaviourTree
+{
+    public interface ITicker
+    {
+
+        Guid Id { get; }
+        string Tag { get; }
+        void GetPath(ref List<Guid> nodes);
+
+        ITicker[] GetChilds();
+
+        void Reset();
+        TICKRESULT Tick(float delta);
+    }
+
+
+}
---
+++ Regulus.QuadTree/IQuadObject.cs
@@ -0,0 +1,12 @@
+﻿using Regulus.Utility;
+using System;
+
+namespace Regulus.Collection
+{
+    public interface IQuadObject
+    {
+        event EventHandler BoundsChanged;
+
+        Rect Bounds { get; }
+    }
+}
